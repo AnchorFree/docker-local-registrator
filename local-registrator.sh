d3414c880906a5862a100e1f6c1eba29fca35e00
Aleksey Zhukov <alex@izhukov.ru>
2017-05-19 13:08:36 +0300

    silent curl

--- local-registrator.sh
+++ local-registrator.sh
@@ -15,7 +15,7 @@ while true; do
             if [ "${VAR:0:${#PREFIX}}" == "$PREFIX" ]; then
                 SERVICE=$(echo "$VAR" | awk -F_ '{print tolower($3)}')
 
-                curl --connect-timeout 10 --max-time 20 -H "Content-Type: application/json" -X PUT \
+                curl -s --connect-timeout 10 --max-time 20 -H "Content-Type: application/json" -X PUT \
                     -d "{\"ID\":\"${SERVICE}:${VAL}\",\"Name\":\"${SERVICE}\",\"Tags\":[\"${TAGS}\"],\"Port\":${VAL},\"Check\":{\"tcp\":\"localhost:${VAL}\",\"Interval\":\"10s\",\"deregister_critical_service_after\":\"1m\"}}" \
                     'http://127.0.0.1:8500/v1/agent/service/register'
             fi
